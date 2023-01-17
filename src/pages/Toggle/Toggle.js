81ec164ce5308801a4efa67cc55d4d07521568aa
hwookim <gusdn9754@gmail.com>
2023-01-17 16:01:30 +0900

    feat: toggle 구현

--- src/pages/Toggle/Toggle.js
+++ src/pages/Toggle/Toggle.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Toggle.scss';
 
 const Toggle = () => {
-  return <>toggle</>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  return (
+    <div className="toggleContainer">
+      <div className="toggleText">
+        <div className="toggleButton" onClick={toggle}>
+          {isOpen ? '▼' : '▶'}
+        </div>
+        숨기고 싶은 내용이 있어요 👀
+      </div>
+      {isOpen && <div className="toggleContents">42기 화이팅!!!!</div>}
+    </div>
+  );
 };
 
 export default Toggle;
