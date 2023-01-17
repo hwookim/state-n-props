ba17d480cbc1e248a78a29f729ab7eac155cca84
hwookim <gusdn9754@gmail.com>
2023-01-18 03:00:41 +0900

    feat: 눈에 편한 색만 남기기

--- src/pages/Color/Color.js
+++ src/pages/Color/Color.js
@@ -4,24 +4,8 @@ import './Color.scss';
 const Color = () => {
   const [color, setColor] = useState('black');
 
-  const handleClickRed = () => {
-    setColor('red');
-  };
-
-  const handleClickOrange = () => {
-    setColor('orange');
-  };
-
-  const handleClickYellow = () => {
-    setColor('yellow');
-  };
-
   const handleClickGreen = () => {
     setColor('green');
-  };
-
-  const handleClickBlue = () => {
-    setColor('blue');
   };
 
   const handleClickPurple = () => {
@@ -34,11 +18,7 @@ const Color = () => {
         <span className="colorName">{color}</span>
       </div>
       <div className="info">
-        <button onClick={handleClickRed}>RED</button>
-        <button onClick={handleClickOrange}>ORANGE</button>
-        <button onClick={handleClickYellow}>YELLOW</button>
         <button onClick={handleClickGreen}>GREEN</button>
-        <button onClick={handleClickBlue}>BLUE</button>
         <button onClick={handleClickPurple}>PURPLE</button>
       </div>
     </div>
