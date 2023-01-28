af825912fb110e0c1dd27302469bcd1c78ee43e9
hwookim <gusdn9754@gmail.com>
2023-01-28 14:21:10 +0900

    Delete: 갈아엎을 준비

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from './pages/Main/Main';
-import Color from './pages/Color/Color';
-import Counter from './pages/Counter/Counter';
-import Toggle from './pages/Toggle/Toggle';
 
 const Router = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/color" element={<Color />} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/toggle" element={<Toggle />} />
+        <Route path="/" element={<>hi</>} />
       </Routes>
     </BrowserRouter>
   );
